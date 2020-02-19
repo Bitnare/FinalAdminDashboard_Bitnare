ca01a8fe9e58d58dc2e7dfdd95bde4ff4f01237e
bibav777 <you@example.com>
2020-02-19 04:14:49 -0800

    Updated handlebars

--- public/js/viewpost.js
+++ public/js/viewpost.js
@@ -1,67 +1,27 @@
 $(document).ready(function() {
 
-    $.ajax({
-        url: 'http://localhost:8000/post',
-        method: 'GET',
-        ContentType: 'application/json',
-        dataType: 'json',
-
-        success: function(result, status) {
-
-            console.log(result.Posts)
-            for (key in result) {
-                $('#card').append('\
-            <tbody>\
-\
-                <tr>\
-                    <td align="center">\
-                        <a class="btn btn-default"><em class="fa fa-pencil"></em></a>\
-                        <a class="btn btn-danger"><em class="fa fa-trash"></em></a>\
-                    </td>\
-                    <td class="hidden-xs">1</td>\
-                    <td>' + result[key].postimage + '</td>\
-                    <td>' + result[key].postdescription + '</td>\
-                    <td>' + result[key].posteddate + '</td>\
-\
-                </tr>\
-\
-            </tbody>\
-\
-\
-            ')
 
 
+    $(".deletepost").click(function(e) {
+        var elem = $(this);
+        e.preventDefault();
+        $.ajax({
+            type: 'DELETE',
+            url: elem.attr("href"),
+            processData: false,
+            contentType: false,
+            success: function(result) {
+                alert('Successfully deleted');
+                window.location.href = 'http://localhost:5000/post/viewpost';
 
-
+            },
+            error: function(err) {
+                alert('error', err)
             }
+        });
 
 
-        },
-
-        error: function(jqXHR, status) {
+    });
 
 
-
-
-        }
-
-
-
-
-
-
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-});
+})
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
-const axios = require('axios')
+const axios = require('axios');
 
 router.get('/addpost', (req, res, next) => {
     res.render('addpost');
@@ -10,26 +10,34 @@ router.get('/addpost', (req, res, next) => {
 });
 
 
-
-
 router.get('/viewpost', (req, res, next) => {
     fetch('http://localhost:8000/post').then(res => {
 
         return res.json();
 
     }).then(json => {
-        console.log(json);
+
         res.render('viewpost', { json });
 
 
     }).catch(err => {
         console.log(err)
     })
+});
 
+router.get('/editpost/:postid', (req, res, next) => {
+    const postid = req.params.postid;
+    fetch('http://localhost:8000/post/' + postid).then(res => {
+        return res.json();
 
+    }).then(json => {
+        console.log(json)
+        res.render('editpost', { json });
 
+    })
 
 
 });
+
 
 module.exports = router;
