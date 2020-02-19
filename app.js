05bf204966f3c03b0a41cca429931cff2f25c3f4
bibav777 <you@example.com>
2020-02-18 23:40:44 -0800

    Server file initiated

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const hbs = require('express-handlebars');
+const path = require('path');
+const postRoutes = require('./routes/postRoutes.js');
+const loginroutes = require('./routes/loginRoutes.js');
+const dashboardRoutes = require('./routes/dashboard.js');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}));
+app.set('view engine', 'handlebars');
+
+app.use('/post', postRoutes);
+app.use('/login', loginroutes);
+app.use('/dashboard', dashboardRoutes);
+
+
+app.listen(5000, () => {
+    console.log("Server running on port 5000")
+
+})
