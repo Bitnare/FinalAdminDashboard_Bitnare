fba376b68663ac384e07f019ec431e0d65837385
poonamlama <poonamlama253@gmail.com>
2020-02-23 10:59:39 +0545

    admin login

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const postRoutes = require('./routes/postRoutes.js');
 const loginroutes = require('./routes/loginRoutes.js');
 const dashboardRoutes = require('./routes/dashboard.js');
+const adminLogin = require('./routes/adminLogin');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -17,6 +18,8 @@ app.set('view engine', 'handlebars');
 app.use('/post', postRoutes);
 app.use('/login', loginroutes);
 app.use('/dashboard', dashboardRoutes);
+app.use('/admin', adminLogin);
+
 
 
 app.listen(5000, () => {
